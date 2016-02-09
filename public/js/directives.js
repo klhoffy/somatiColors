55ecc00bdd928ea3612edbd2cb2568cf39153842
Jessica Ganji <jessicaganji@gmail.com>
2016-02-09 12:23:40 -0800

    restructures directives and adds to partials

--- public/js/directives.js
+++ public/js/directives.js
@@ -14,80 +14,93 @@ angular.module('SomatiColors')
     
 // Index Directives //'A' == attribute, 'E' == element, 'C' == class
 function nav (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_nav.html";
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "../partials/_nav.html"
+  };
   return directive;
 }
 
 function footer (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
 
 // User Directives //'A' == attribute, 'E' == element, 'C' == class
 function allUsers (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
+
 function oneUser (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
+
 function newUserForm (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
+
 function editUserForm (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
 
 // Event Directives //'A' == attribute, 'E' == element, 'C' == class
 function allEvents (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
 
 function oneEvent (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
 
 function newEventForm (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
 
 function editEventForm (){
-  var directive = {};
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_footer.html";
-  return directive; 
+  var directive = {
+  restrict: 'E',
+  replace: true,
+  templateUrl:  "_footer.html"
+  };
+  return directive;
 }
