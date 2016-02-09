ee97073fab3ab3dfe7ff53f8b2a40ff3bdd34d44
Jessica Ganji <jessicaganji@gmail.com>
2016-02-08 20:53:09 -0800

    adds to event model

--- models/event.js
+++ models/event.js
@@ -2,7 +2,24 @@ var mongoose = require('mongoose');
 
 var Event = mongoose.Schema({	
 	title: String,
-    location: String,
+	date: String,
+	people_involved: String,
+	location: String,
+	situation: String,
+	triggers: String,
+	automatic_thoughts: String,
+	rational_response : String,
+	outcome: String,
+	bodily_sensations: String,
+	emotions: String,
+	behaviors: String,
+	consequences: String,
+	challanged_core_beliefs: String,
+	fears: String,
+	old_perspective: String,
+	new_perspective: String,
+	lessons: String,
+	coping_strategies: String,
     user_id: String
 });
 
