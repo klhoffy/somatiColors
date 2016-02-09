56470b2dc92ff7bd29b5bf99a94b7601ee1d31a6
Jessica Ganji <jessicaganji@gmail.com>
2016-02-09 10:09:43 -0800

    adds derectives and sorts partials

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,12 +1,19 @@
 angular.module('SomatiColors')
     .config(MainRouter)
-    
+    .directive('nav-top', nav)
+    .directive('footer-bottom', footer)
+        
 function MainRouter ($stateProvider, $urlRouterProvider){
     $stateProvider
         
         .state('users', {
             url: '/',
             templateUrl: 'users.html'
+        })
+        
+        .state('events', {
+            url: '/',
+            templateUrl: 'events.html'
         })
 
         .state('about', {
@@ -16,4 +23,22 @@ function MainRouter ($stateProvider, $urlRouterProvider){
     
     $urlRouterProvider.otherwise('/');
     
+}
+
+function nav (){
+  var directive = {};
+  //'A' == attribute, 'E' == element, 'C' == class
+  directive.restrict = 'E';
+  directive.replace = true;
+  directive.templateUrl =  "_nav.html";
+  return directive;
+}
+
+function footer (){
+  var directive = {};
+  //'A' == attribute, 'E' == element, 'C' == class
+  directive.restrict = 'E';
+  directive.replace = true;
+  directive.templateUrl =  "_footer.html";
+  return directive; 
 }
