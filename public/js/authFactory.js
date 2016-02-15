0a11c8ec7c8a5e3cd292722071eb4386b3f30873
Kristin Hoffer <kristinlhoffer@gmail.com>
2016-02-15 12:28:45 -0800

    change username to email

--- public/js/authFactory.js
+++ public/js/authFactory.js
@@ -56,18 +56,18 @@ function authFactory($http, $q, authTokenFactory, $window){
         return $http.get('http://localhost:3000/api/users')
     }
     // handle login
-    authFactory.login = function(username, password){
+    authFactory.login = function(email, password){
         return $http.post('http://localhost:3000/api/authenticate', {
-            username: username,
+            email: email,
             password: password
         }).then(function(response){
             authTokenFactory.setToken(response.data.token)
             return response
         })
     }
-    authFactory.signup = function(username, password){
+    authFactory.signup = function(email, password){
         return $http.post('http://localhost:3000/api/users', {
-            username: username,
+            email: email,
             password: password
         })
     }
