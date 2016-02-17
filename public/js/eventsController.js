3586d15fd84049c1671e99e884da3d4c4b0930f0
Miwa Tachibana <tachibam@gmail.com>
2016-02-17 13:25:35 -0800

    gets events to show on the timeline

--- public/js/eventsController.js
+++ public/js/eventsController.js
@@ -1,10 +1,27 @@
 angular.module('SomatiColors')
 	.controller('EventsController', EventsController)
 
-EventsController.$inject=['eventsFactory','$stateParams','$location']
+EventsController.$inject=['$stateParams','$location', '$http']
 
-function EventsController(eventsFactory,$stateParams,$location){
+function EventsController($stateParams,$location, $http){
 	var vm = this
-    vm.params = $stateParams.user_id	
+	vm.params = $stateParams.user_id	
+
+   	vm.events = [];
+      vm.getEventsAPI = getEventsAPI;
+
+    	function getEventsAPI(user_id){
+    	$http
+        .get('http://localhost:3000/api/users/' +  user_id + '/events')
+        .then(function(response){
+            vm.events = response.data.events;
+            console.log( response.data.events)
+            // console.log( vm.events[0].title )
+            
+        });
+    }
+
+
+
 
 }
