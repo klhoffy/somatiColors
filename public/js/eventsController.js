270af56492c0c65eb3a85065b630d2de64b3584b
Miwa Tachibana <tachibam@gmail.com>
2016-02-18 09:44:19 -0800

    removes all the event stuff from my code

--- public/js/eventsController.js
+++ public/js/eventsController.js
@@ -7,21 +7,6 @@ function EventsController($stateParams,$location, $http){
 	var vm = this
 	vm.params = $stateParams.user_id	
 
-   	// vm.events = [];
-    //   vm.getEventsAPI = getEventsAPI;
-
-    // 	function getEventsAPI(user_id){
-    // 	$http
-    //     .get('http://localhost:3000/api/users/' +  user_id + '/events')
-    //     .then(function(response){
-    //         vm.events = response.data.events;
-    //         console.log( response.data.events)
-    //         // console.log( vm.events[0].title )
-            
-    //     });
-    // }
-
-
 
 
 }
