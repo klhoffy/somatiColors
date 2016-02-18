b5037b3fe52b15461384c0ddf9e55b2bdfcc368f
Kristin Hoffer <kristinlhoffer@gmail.com>
2016-02-18 00:31:14 -0800

    update event works with front end and api

--- public/js/eventsController.js
+++ public/js/eventsController.js
@@ -62,12 +62,15 @@ function EventsController(eventsFactory, $stateParams, $location, $http){
     return $http
         .put('http://localhost:3000/api/users/' + user_id + '/events/' + event_id, vm.updatedEventInfo)
         .then(function(response){
-            vm.eventInfo = response.data.event;
-            console.log(vm.eventInfo)
+            // vm.eventInfo = response.data.event;
+            // console.log(vm.eventInfo)
+            console.log(event_id)
             vm.updatedEventInfo = response.data.event;
+            
             console.log(vm.updatedEventInfo)
             vm.editing = false;
             console.log(response)
+            getEventsAPI(vm.params)
         });
     }
 }
