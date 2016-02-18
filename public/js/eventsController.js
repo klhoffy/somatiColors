9417f27079f4ff3625e9bd889b686d45df283ab0
Miwa Tachibana <tachibam@gmail.com>
2016-02-17 19:20:39 -0800

    adds delete user functionality

--- public/js/eventsController.js
+++ public/js/eventsController.js
@@ -7,19 +7,19 @@ function EventsController($stateParams,$location, $http){
 	var vm = this
 	vm.params = $stateParams.user_id	
 
-   	vm.events = [];
-      vm.getEventsAPI = getEventsAPI;
+   	// vm.events = [];
+    //   vm.getEventsAPI = getEventsAPI;
 
-    	function getEventsAPI(user_id){
-    	$http
-        .get('http://localhost:3000/api/users/' +  user_id + '/events')
-        .then(function(response){
-            vm.events = response.data.events;
-            console.log( response.data.events)
-            // console.log( vm.events[0].title )
+    // 	function getEventsAPI(user_id){
+    // 	$http
+    //     .get('http://localhost:3000/api/users/' +  user_id + '/events')
+    //     .then(function(response){
+    //         vm.events = response.data.events;
+    //         console.log( response.data.events)
+    //         // console.log( vm.events[0].title )
             
-        });
-    }
+    //     });
+    // }
 
 
 
