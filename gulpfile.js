5af740b3951a70833d3f063d5b1cf2902eb5711a
Miwa Tachibana <tachibam@gmail.com>
2016-02-18 19:16:02 -0800

    fixes gulp bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,44 @@
-// var 
-// 	gulp = require('gulp'),
-// 	concat = require('gulp-concat'),
-// 	uglify = require('gulp-uglify'),
-// 	uglifycss = require('gulp-uglifycss'),
-// 	nodemon = require('gulp-nodemon'),
-// 	browserSync = require('browser-sync')
+var 
+	gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	uglifycss = require('gulp-uglifycss'),
+	nodemon = require('gulp-nodemon'),
+	browserSync = require('browser-sync')
 
 
-// ////////////////////////////////////////// ///////////////////// minify all css files ///////////////////////////////////////////////////////////////
-// gulp.task('minify-css', function() {
-//     gulp.src('public-dev/css/*.css')
-//         .pipe(concat('application.min.css'))
-//         .pipe(uglifycss() )
-//         .pipe(gulp.dest('public/css'))
-//         .pipe(browserSync.stream() )
-// })
+////////////////////////////////////////// ///////////////////// minify all css files ///////////////////////////////////////////////////////////////
+gulp.task('minify-css', function() {
+    gulp.src('public-dev/css/*.css')
+        .pipe(concat('application.min.css'))
+        .pipe(uglifycss() )
+        .pipe(gulp.dest('public/css'))
+        // .pipe(browserSync.stream() )
+})
 
-// /////////////////////////////////////////////////////////////// run nodemon ///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////// run nodemon ///////////////////////////////////////////////////////////////
 
-// gulp.task('nodemon', function(){
-//     nodemon({
-//         ext: "js html css",
-//         env: {'NODE_ENV': 'development'}
-//     })
-// })
+gulp.task('nodemon', function(){
+    nodemon({
+        ext: "js html css",
+        env: {'NODE_ENV': 'development'}
+    })
+})
 
-// ////////////////////////////////////////////////////////// browser sync & nodemon //////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////// browser sync & nodemon //////////////////////////////////////////////////////////
 
-// gulp.task('browser-sync', ['nodemon'], function() {
-//     browserSync.init(null, {
-//         proxy: "http://localhost:3000",
-//         files: ["public-dev/**/*.*"],
-//         browser: "google chrome",
-//         port: 7000,
-//     });
-// });
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["public-dev/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
+    });
+});
 
 
-// gulp.watch('public-dev/css/*.css', ['minify-css'])
+gulp.watch('public-dev/css/*.css', ['minify-css'])
 
-// gulp.task('default', ['browser-sync'], function() {
-//     console.log("gulp gulp gulp")
-// })
-    
+gulp.task('default', ['browser-sync'], function() {
+    console.log("gulp gulp gulp")
+})
